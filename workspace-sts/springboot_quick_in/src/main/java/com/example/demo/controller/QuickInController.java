22ff4b57661f355ad6e036afbae8312287ca2798
JesusisLove <liu_ym@hotmail.com>
2020-07-10 14:04:27 +0900

    学习了@Value @ConfigurationProperties与XXX.yml的配合使用

--- workspace-sts/springboot_quick_in/src/main/java/com/example/demo/controller/QuickInController.java
+++ workspace-sts/springboot_quick_in/src/main/java/com/example/demo/controller/QuickInController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuickInController {
-	
 	@RequestMapping("/quickin")
 	@ResponseBody
 	public String quickIn() {
