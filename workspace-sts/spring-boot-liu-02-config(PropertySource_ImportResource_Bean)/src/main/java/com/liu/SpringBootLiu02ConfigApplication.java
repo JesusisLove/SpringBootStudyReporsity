c39e462c865e3e346212a0c50b0dbc311fce443f
JesusisLove <liu_ym@hotmail.com>
2020-07-13 23:03:11 +0900

    学习SpringBoot之 @PropertySource,@ImportResource,@Bean（二）SpringBoot推荐使用@Bean注解来使用用户自己写配置类去取代以前Spring框架中用户自定义的XML配置文件(beans.xml)

--- workspace-sts/spring-boot-liu-02-config(PropertySource_ImportResource_Bean)/src/main/java/com/liu/SpringBootLiu02ConfigApplication.java
+++ workspace-sts/spring-boot-liu-02-config(PropertySource_ImportResource_Bean)/src/main/java/com/liu/SpringBootLiu02ConfigApplication.java
@@ -10,17 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 /*
  * @SpringBootApplication：标注主程序类，说明这是一个spring boot应用的一个主配置类
  * */
-@ImportResource (locations = {"classpath:beans.xml"}) //⭐⭐⭐知识点️️️️导入Spring的配置文件，让自定义的XML文件（如beans.xml）内容生效；
 @SpringBootApplication
 @RestController
+/* 学习SpringBoot之 @PropertySource,@ImportResource,@Bean（二）
+ *  SpringBoot不推荐使用xml配置文件来配置容器组件，推荐用户写配置类：参考用户写的配置类 --> MyAppConfig.java
+ *  @ImportResource (locations = {"classpath:beans.xml"}) //⭐⭐⭐知识点️️️️导入Spring的配置文件，让自定义的XML文件（如beans.xml）内容生效；
+*/
 public class SpringBootLiu02ConfigApplication {
 
 	public static void main(String[] args) {
 		// 启动spring应用
 		SpringApplication.run(SpringBootLiu02ConfigApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
 	}
 }
