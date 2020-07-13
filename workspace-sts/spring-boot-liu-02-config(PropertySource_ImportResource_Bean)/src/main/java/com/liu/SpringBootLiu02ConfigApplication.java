851cb36fad9e1685b03c79eb6f9368dad05c066e
JesusisLove <liu_ym@hotmail.com>
2020-07-13 21:25:40 +0900

    学习SpringBoot之 @PropertySource,@ImportResource,@Bean（一）

---
+++ workspace-sts/spring-boot-liu-02-config(PropertySource_ImportResource_Bean)/src/main/java/com/liu/SpringBootLiu02ConfigApplication.java
@@ -0,0 +1,26 @@
+package com.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * @SpringBootApplication：标注主程序类，说明这是一个spring boot应用的一个主配置类
+ * */
+@ImportResource (locations = {"classpath:beans.xml"}) //⭐⭐⭐知识点️️️️导入Spring的配置文件，让自定义的XML文件（如beans.xml）内容生效；
+@SpringBootApplication
+@RestController
+public class SpringBootLiu02ConfigApplication {
+
+	public static void main(String[] args) {
+		// 启动spring应用
+		SpringApplication.run(SpringBootLiu02ConfigApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}
+}
---
+++ workspace-sts/spring-boot-liu-02-config(PropertySource_ImportResource_Bean)/src/main/java/com/liu/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.liu.service;
+
+public class HelloService {
+
+}
