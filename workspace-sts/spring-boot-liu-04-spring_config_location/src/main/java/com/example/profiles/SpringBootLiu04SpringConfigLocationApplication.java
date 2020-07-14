65a33085803123f557633d705994adb55ae8058f
JesusisLove <liu_ym@hotmail.com>
2020-07-14 21:41:39 +0900

    2020/07/14

--- workspace-sts/spring-boot-liu-04-spring_config_location/src/main/java/com/example/profiles/SpringBootLiu04SpringConfigLocationApplication.java
+++ workspace-sts/spring-boot-liu-04-spring_config_location/src/main/java/com/example/profiles/SpringBootLiu04SpringConfigLocationApplication.java
@@ -9,7 +9,6 @@ public class SpringBootLiu04SpringConfigLocationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLiu04SpringConfigLocationApplication.class, args);
 	}
-
 }
 
 /* ⭐⭐⭐⭐⭐⭐️尚硅谷 SpringBoot配置文件加载位置 
@@ -19,8 +18,18 @@ public class SpringBootLiu04SpringConfigLocationApplication {
  * 			②　-- file:./
  * 			③　-- classpath:./config/
  * 			④　-- classpath:./
+ * 		🔸SpringBoot会从这四个位置全部加载主配置文件(application.properties application.yml),且配置互补（即，取这四个地方的文件内容的并集）
  * 
- * ⭐️　我们也可以通过【spring.config.location】来改变默认配置
+ * ⭐️　我们也可以通过【spring.config.location】来改变配置文件的默认配置位置
+ *  		🔸spring.config.location的使用timing：（在项目运维的阶段补充配置文件的时候经常用到）
+ *  			项目打包好以后，我们可以使用命令行参数的形式，启动项目的时候来指定配置文件的新位置；
+ *  			指定新位置的配置文件会和默认加载的这些配置文件一起作用，形成互补配置。
  * */
-
+// 教学参考视频
 /* https://www.youtube.com/watch?v=iufbdW2wsmM&list=PLmOn9nNkQxJEFsK2HVO9-WA55Z7LZ2N0S&index=17 */
+
+/* ⭐⭐⭐⭐⭐⭐️尚硅谷 SpringBoot外部配置加载顺序 
+ *  参看SrpingBoot官方网站文档：https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/htmlsingle/#boot-features-external-config
+ */
+//教学参考视频
+// https://www.youtube.com/watch?v=mIJzlAuWk2w&list=PLmOn9nNkQxJEFsK2HVO9-WA55Z7LZ2N0S&index=18
