6d92e2e341fbec55d1cc2a8d7c23c322d0651d72
JesusisLove <liu_ym@hotmail.com>
2020-07-16 19:43:25 +0900

    自动配置原理

---
+++ workspace-sts/spring-boot-liu-05-autoconfig/src/main/java/com/liu/SpringBootLiu05AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiu05AutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiu05AutoconfigApplication.class, args);
+	}
+
+}
