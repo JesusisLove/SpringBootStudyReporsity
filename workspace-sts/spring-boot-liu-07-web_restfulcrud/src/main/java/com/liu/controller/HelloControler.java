a7e2e72630a07f12cd9069f40f21ddbfe9ab0dba
JesusisLove <liu_ym@hotmail.com>
2020-07-22 11:55:43 +0900

    添加一个success的html文件，初次体验thymeleaf模版引擎的语法

--- workspace-sts/spring-boot-liu-07-web_restfulcrud/src/main/java/com/liu/controller/HelloControler.java
+++ workspace-sts/spring-boot-liu-07-web_restfulcrud/src/main/java/com/liu/controller/HelloControler.java
@@ -1,5 +1,7 @@
 package com.liu.controller;
 
+import java.util.Arrays;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,8 +19,9 @@ public class HelloControler {
 	
 //	@ResponseBody
 	@RequestMapping("success")
-	public String success() {
-		
+	public String success(Map<String,Object> map) {
+		map.put("hi", "<h1>你好👋</h1>");
+		map.put("users", Arrays.asList( "zhang san","li si","wang wu"));
 		// classpath:/templates/success.html
 		return "success";
 	}
